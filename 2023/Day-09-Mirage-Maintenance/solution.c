1c735b6ef63764d0ba1264f879fd31ce80425d03
Nikolay Chumerin (TME) <nikolay.chumerin@toyota-europe.com>
2023-12-09 15:41:48 +0100

    Add D09P1 solution

--- 2023/Day-09-Mirage-Maintenance/solution.c
+++ 2023/Day-09-Mirage-Maintenance/solution.c
@@ -22,8 +22,9 @@
 #define SEQUENCE_CAPACITY (1024)
 #define SEQUENCES_MAX_NUM (256)
 
+typedef int seq_data_t[SEQUENCE_CAPACITY];
 typedef struct {
-  int seq[SEQUENCE_CAPACITY];
+  seq_data_t data;
   size_t len;
 } seq_t;
 
@@ -57,11 +58,14 @@ int read_input_data(const char *input_file_path) {
     int chars_read = 0;
     char *str = line;
     seq_t *seq = &seqs[seqs_num];
+    int *data = (int *)seq->data;
     seq->len = 0U;
     do {
-      vals_read = sscanf(str, "%d %n", &seq->seq[seq->len], &chars_read);
+      vals_read = sscanf(str, "%i %n", data, &chars_read);
       if (vals_read != 1) break;
+      PRINTF("%d ", seqs[seqs_num].data[seqs[seqs_num].len]);
       ++seq->len;
+      ++data;
       str += chars_read;
     } while (true);
     ++seqs_num;
@@ -72,9 +76,46 @@ int read_input_data(const char *input_file_path) {
   return EXIT_SUCCESS;
 } /* read_input_data(.) */
 /******************************************************************************/
+void print_seq(const seq_t *seq) {
+  PRINTF("len=%lu [ ", seq->len);
+  const int *d = seq->data;
+  for (size_t i = 0U; (i < seq->len); ++i) PRINTF("%d ", *d++);
+  PRINTF("]\n");
+}
+/******************************************************************************/
+bool all_zeros(const seq_t *seq) {
+  bool ans = true;
+  const int *data = seq->data;
+  for (size_t i = 0U; ans && (i < seq->len); ++i) ans &= (data[i] == 0);
+  return ans;
+} /* all_zeros(.) */
+/******************************************************************************/
 void solve_part1(void) {
-  /* solution of the part1 */
-  printf("%d\n", 0);
+  int ans = 0;
+  seq_t diffs;
+  for (size_t seq_idx = 0U; seq_idx < seqs_num; ++seq_idx) {
+    const seq_t *seq = &seqs[seq_idx];
+    const int *data = (int *)seq->data;
+    const size_t seq_len = seq->len;
+    diffs = seqs[seq_idx];  // memmove(&diffs, seq, sizeof(seq_t));
+    print_seq(&diffs);
+    do {
+      for (size_t i = 0; i < diffs.len - 1; ++i) {
+        diffs.data[i] = diffs.data[i + 1] - diffs.data[i];
+      }
+      --diffs.len;
+      print_seq(&diffs);
+    } while (!all_zeros(&diffs));
+
+    int seq_next_element = 0;
+    for (size_t i = diffs.len - 1; i < seq_len; ++i) {
+      seq_next_element += diffs.data[i];
+    }
+    PRINTF("seq_next_element=%d\n", seq_next_element);
+    ans += seq_next_element;
+  } /* loop over sequences */
+
+  printf("%d\n", ans);
 } /* solve_part1() */
 /******************************************************************************/
 void solve_part2(void) {
