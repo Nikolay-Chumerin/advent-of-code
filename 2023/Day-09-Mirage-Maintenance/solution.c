07511bbe2ff3740bd4b2e1ebd14bc2f6d7204dc6
Nikolay Chumerin <chumerin@gmail.com>
2023-12-10 11:05:11 +0100

    Add D09P2 solution

--- 2023/Day-09-Mirage-Maintenance/solution.c
+++ 2023/Day-09-Mirage-Maintenance/solution.c
@@ -11,7 +11,7 @@
 #define EPS (1E-8)
 #define DEBUG
 
-#ifdef DEBUG
+#ifdef DEBUG_
 #define PUTS puts
 #define PRINTF printf
 #else
@@ -19,10 +19,11 @@
 #define PRINTF(fmt, ...) (0)
 #endif  // DEBUG
 
-#define SEQUENCE_CAPACITY (1024)
+#define SEQUENCE_CAPACITY (32)
 #define SEQUENCES_MAX_NUM (256)
 
-typedef int seq_data_t[SEQUENCE_CAPACITY];
+typedef int64_t val_t;
+typedef val_t seq_data_t[SEQUENCE_CAPACITY];
 typedef struct {
   seq_data_t data;
   size_t len;
@@ -31,6 +32,17 @@ typedef struct {
 seq_t seqs[SEQUENCES_MAX_NUM];
 size_t seqs_num;
 
+/******************************************************************************/
+#ifdef DEBUG
+void print_seq(const seq_t *seq) {
+  PRINTF("len=%lu [ ", seq->len);
+  const val_t *d = seq->data;
+  for (size_t i = 0U; (i < seq->len); ++i) PRINTF("%02ld ", *d++);
+  PRINTF("]\n");
+} /* print_seq(.) */
+#else
+#define print_seq(a) (0)
+#endif /* DEBUG */
 /******************************************************************************/
 void trim(char *line) {
   int line_len = strlen(line);
@@ -52,52 +64,48 @@ int read_input_data(const char *input_file_path) {
   while (true) {
     if (!fgets(line, sizeof(line), file)) break;
     trim(line);
-    PRINTF("'%s'\n", line);
-    /* process line ... */
+    // PRINTF("'%s'\n", line);
     int vals_read = 0;
     int chars_read = 0;
     char *str = line;
     seq_t *seq = &seqs[seqs_num];
-    int *data = (int *)seq->data;
+    val_t *data = (val_t *)seq->data;
     seq->len = 0U;
     do {
-      vals_read = sscanf(str, "%i %n", data, &chars_read);
+      vals_read = sscanf(str, "%ld %n", data, &chars_read);
       if (vals_read != 1) break;
-      PRINTF("%d ", seqs[seqs_num].data[seqs[seqs_num].len]);
+      // PRINTF("%ld ", seqs[seqs_num].data[seqs[seqs_num].len]);
       ++seq->len;
       ++data;
       str += chars_read;
     } while (true);
+    print_seq(seq);
     ++seqs_num;
-    PRINTF(" Done: %lu values were read.\n", seq->len);
+    // PRINTF(" Done: %lu values were read.\n", seq->len);
   } /* loop over lines */
   fclose(file);
   PRINTF("Input data (%lu sequences) were read.\n", seqs_num);
   return EXIT_SUCCESS;
 } /* read_input_data(.) */
 /******************************************************************************/
-void print_seq(const seq_t *seq) {
-  PRINTF("len=%lu [ ", seq->len);
-  const int *d = seq->data;
-  for (size_t i = 0U; (i < seq->len); ++i)
-    PRINTF("%02d ", *d++);
-  PRINTF("]\n");
-}
-/******************************************************************************/
-bool all_zeros(const int* data, const size_t len) {
+bool all_zeros(const val_t *data, const size_t len) {
   bool ans = true;
   for (size_t i = 0U; ans && (i < len); ++i) ans &= (data[i] == 0);
   return ans;
 } /* all_zeros(.) */
 /******************************************************************************/
 void solve_part1(void) {
-  int ans = 0;
+  val_t ans = 0;
   seq_t diffs;
   for (size_t seq_idx = 0U; seq_idx < seqs_num; ++seq_idx) {
+    PRINTF("---[seq_idx=%lu]----------------\n", seq_idx);
     const seq_t *seq = &seqs[seq_idx];
-    const int *data = (int *)seq->data;
+    const val_t *data = (val_t *)seq->data;
     const size_t seq_len = seq->len;
     diffs = seqs[seq_idx];  // memmove(&diffs, seq, sizeof(seq_t));
+    PRINTF("seq:");
+    print_seq(seq);
+    PRINTF("diffs:");
     print_seq(&diffs);
     do {
       for (size_t i = 0; i < diffs.len - 1; ++i) {
@@ -107,25 +115,30 @@ void solve_part1(void) {
       print_seq(&diffs);
     } while (!all_zeros(diffs.data, diffs.len));
 
-    int seq_next_element = 0;
+    val_t seq_next_element = 0;
     for (size_t i = diffs.len - 1; i < seq_len; ++i) {
+      PRINTF("seq_next_element=%ld\n", seq_next_element);
       seq_next_element += diffs.data[i];
     }
-    PRINTF("seq_next_element=%d\n", seq_next_element);
+    PRINTF("seq_next_element=%ld\n", seq_next_element);
     ans += seq_next_element;
   } /* loop over sequences */
 
-  printf("%d\n", ans);
+  printf("%ld\n", ans);
 } /* solve_part1() */
 /******************************************************************************/
 void solve_part2(void) {
-  int ans = 0;
+  val_t ans = 0;
   seq_t diffs;
   for (size_t seq_idx = 0U; seq_idx < seqs_num; ++seq_idx) {
+    PRINTF("---[seq_idx=%lu]----------------\n", seq_idx);
     const seq_t *seq = &seqs[seq_idx];
-    const int *data = (int *)seq->data;
+    const val_t *data = (val_t *)seq->data;
     const size_t seq_len = seq->len;
     diffs = seqs[seq_idx];
+    PRINTF("seq:");
+    print_seq(seq);
+    PRINTF("diffs:");
     print_seq(&diffs);
     bool diffs_are_all_zeros = false;
     size_t step = 0;
@@ -140,15 +153,14 @@ void solve_part2(void) {
     } while (!diffs_are_all_zeros);
 
     PRINTF("step=%lu\n", step);
-    int seq_prev_element = 0;
-    for (size_t i = 0; i <= seq_len - step + 1; ++i) {
+    val_t seq_prev_element = 0;
+    for (int i = step; i >= 0; --i) {
       seq_prev_element = diffs.data[i] - seq_prev_element;
+      PRINTF("seq_prev_element=%ld\n", seq_prev_element);
     }
-    seq_prev_element *= -1;
-    PRINTF("seq_prev_element=%d\n", seq_prev_element);
     ans += seq_prev_element;
   } /* loop over sequences */
-  printf("%d\n", ans);
+  printf("%ld\n", ans);
 } /* solve_part2() */
 /******************************************************************************/
 int main(int argc, char *argv[]) {
@@ -157,9 +169,7 @@ int main(int argc, char *argv[]) {
     perror("Error reading input data from file");
     return (EXIT_FAILURE);
   }
-
   solve_part1();
   solve_part2();
-
   return EXIT_SUCCESS;
 }
