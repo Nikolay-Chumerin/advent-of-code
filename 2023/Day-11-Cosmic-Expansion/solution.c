65ab75eb8fadf34767a8c3139b310a27a542bd39
Nikolay Chumerin (TME) <nikolay.chumerin@toyota-europe.com>
2023-12-11 14:06:38 +0100

    Add D11P2 solution

--- 2023/Day-11-Cosmic-Expansion/solution.c
+++ 2023/Day-11-Cosmic-Expansion/solution.c
@@ -23,14 +23,15 @@
 
 #define GALAXY_CHAR '#'
 typedef struct {
-  int row;
-  int col;
+  uint64_t row;
+  uint64_t col;
 } pos_t;
 #define GALAXIES_MAX_NUM (512)
 pos_t galaxy_positions[GALAXIES_MAX_NUM] = {};
+pos_t galaxy_new_positions[GALAXIES_MAX_NUM] = {};
 int galaxies_num = 0;
-int map_cols_num = 0;
-int map_rows_num = 0;
+uint64_t map_cols_num = 0;
+uint64_t map_rows_num = 0;
 bool empty_cols_mask[MAP_MAX_WIDTH] = {true};
 bool empty_rows_mask[MAP_MAX_WIDTH] = {true};
 /******************************************************************************/
@@ -58,7 +59,7 @@ int read_input_data(const char *input_file_path) {
     trim(line);
     if (!map_cols_num) map_cols_num = strlen(line);
     bool maps_row_is_empty = true;
-    for (int col_idx = 0; col_idx < map_cols_num; ++col_idx) {
+    for (uint64_t col_idx = 0; col_idx < map_cols_num; ++col_idx) {
       if (GALAXY_CHAR == line[col_idx]) {
         galaxy_positions[galaxies_num].row = map_rows_num;
         galaxy_positions[galaxies_num].col = col_idx;
@@ -76,25 +77,31 @@ int read_input_data(const char *input_file_path) {
   return EXIT_SUCCESS;
 } /* read_input_data(.) */
 /******************************************************************************/
-void expand_the_universe(void) {
+void expand_the_universe(const uint64_t expansion_rate) {
   int cols_new_indices[MAP_MAX_WIDTH];
   int rows_new_indices[MAP_MAX_HEIGHT];
-  int j = 0;
-  for (int i = 0; i < map_cols_num; ++i) {
+  uint64_t j;
+  j = 0U;
+  const uint64_t d = expansion_rate - 1;
+  for (uint64_t i = 0; i < map_cols_num; ++i) {
     cols_new_indices[i] = j++;
-    if (empty_cols_mask[i]) ++j;
+    if (empty_cols_mask[i]) {
+      j += d;
+    }
   }
-  j = 0;
-  for (int i = 0; i < map_rows_num; ++i) {
+  j = 0U;
+  for (uint64_t i = 0; i < map_rows_num; ++i) {
     rows_new_indices[i] = j++;
-    if (empty_rows_mask[i]) ++j;
+    if (empty_rows_mask[i]) {
+      j += d;
+    }
   }
 
   for (int i = 0; i < galaxies_num; ++i) {
     const pos_t old_pos = galaxy_positions[i];
     const pos_t new_pos = {.col = cols_new_indices[old_pos.col],
                            .row = rows_new_indices[old_pos.row]};
-    galaxy_positions[i] = new_pos;
+    galaxy_new_positions[i] = new_pos;
     PRINTF("Galaxy#%03d: (%03d, %03d) -> (%03d, %03d)\n", i, old_pos.row,
            old_pos.col, new_pos.row, new_pos.col);
   } /* loop over galaxies */
@@ -102,22 +109,30 @@ void expand_the_universe(void) {
 } /* expand_the_universe() */
 /******************************************************************************/
 #define ABS(x) ((x) < 0 ? -(x) : (x))
-void solve_part1(void) {
-  long sum = 0;
+uint64_t sum_distances(void) {
+  uint64_t sum = 0;
   for (int i = 0; i < galaxies_num - 1; ++i) {
-    const pos_t pi = galaxy_positions[i];
+    const pos_t pi = galaxy_new_positions[i];
     for (int j = i + 1; j < galaxies_num; ++j) {
-      const pos_t pj = galaxy_positions[j];
-      const long distance = ABS(pi.row - pj.row) + ABS(pi.col - pj.col);
+      const pos_t pj = galaxy_new_positions[j];
+      uint64_t distance = 0U;
+      distance += (pi.row < pj.row) ? pj.row - pi.row : pi.row - pj.row;
+      distance += (pi.col < pj.col) ? pj.col - pi.col : pi.col - pj.col;
       sum += distance;
     } /* j-loop */
-  }   /* i-loop */
-  printf("%ld\n", sum);
+    PRINTF("sum = %lu\n", sum);
+  } /* i-loop */
+  return sum;
+} /* sum_distances(void) */
+/******************************************************************************/
+void solve_part1(void) {
+  expand_the_universe(2);
+  printf("%lu\n", sum_distances());
 } /* solve_part1() */
 /******************************************************************************/
 void solve_part2(void) {
-  /* solution of the part2 */
-  printf("%d\n", 0);
+  expand_the_universe(1000000);
+  printf("%lu\n", sum_distances());
 } /* solve_part2() */
 /******************************************************************************/
 int main(int argc, char *argv[]) {
@@ -126,7 +141,6 @@ int main(int argc, char *argv[]) {
     perror("Error reading input data from file");
     return (EXIT_FAILURE);
   }
-  expand_the_universe();
   solve_part1();
   solve_part2();
   return EXIT_SUCCESS;
