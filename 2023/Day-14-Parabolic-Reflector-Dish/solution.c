5969484eccedc223e6a82a99addeb743e9ff8e1a
Nikolay Chumerin (TME) <nikolay.chumerin@toyota-europe.com>
2023-12-18 00:43:25 +0100

    Add D14P2 solution

--- 2023/Day-14-Parabolic-Reflector-Dish/solution.c
+++ 2023/Day-14-Parabolic-Reflector-Dish/solution.c
@@ -22,6 +22,7 @@
 #endif // DEBUG
 
 char dish[ROWS_MAX_NUM][COLS_MAX_NUM] = {0};
+char dish1[ROWS_MAX_NUM][COLS_MAX_NUM] = {0};
 size_t rows_num = 0;
 size_t cols_num = 0;
 
@@ -86,37 +87,121 @@ void tilt_dish_north(void) {
   }   /* loop over columns */
 } /* tilt_dish_north() */
 /******************************************************************************/
+void tilt_dish_south(void) {
+  for (size_t col_idx = 0; col_idx < cols_num; ++col_idx) {
+    size_t j = rows_num - 1;
+    for (int row_idx = j; row_idx >= 0; --row_idx) {
+      const char c = dish[row_idx][col_idx];
+      if ('.' == c)
+        continue;
+      if ('O' == c) {
+        dish[row_idx][col_idx] = '.';
+        while (j && '.' != dish[j][col_idx])
+          --j;
+        dish[j--][col_idx] = c;
+      } else { /* '#' == c */
+        j = row_idx - 1;
+      }
+    } /* loop over rows */
+  }   /* loop over columns */
+} /* tilt_dish_south() */
+/******************************************************************************/
+void tilt_dish_west(void) {
+  for (size_t row_idx = 0; row_idx < rows_num; ++row_idx) {
+    size_t j = 0;
+    for (size_t col_idx = 0; col_idx < cols_num; ++col_idx) {
+      const char c = dish[row_idx][col_idx];
+      if ('.' == c)
+        continue;
+      if ('O' == c) {
+        dish[row_idx][col_idx] = '.';
+        while ('.' != dish[row_idx][j])
+          ++j;
+        dish[row_idx][j++] = c;
+      } else { /* '#' == c */
+        j = col_idx + 1;
+      }
+    } /* loop over columns */
+  }   /* loop over rows */
+} /* tilt_dish_west() */
+/******************************************************************************/
+void tilt_dish_east(void) {
+  for (size_t row_idx = 0; row_idx < rows_num; ++row_idx) {
+    size_t j = cols_num - 1;
+    for (int col_idx = j; col_idx >= 0; --col_idx) {
+      const char c = dish[row_idx][col_idx];
+      if ('.' == c)
+        continue;
+      if ('O' == c) {
+        dish[row_idx][col_idx] = '.';
+        while (j && '.' != dish[row_idx][j])
+          ++j;
+        dish[row_idx][j--] = c;
+      } else { /* '#' == c */
+        j = col_idx - 1;
+      }
+    } /* loop over columns */
+  }   /* loop over rows */
+} /* tilt_dish_east() */
+/******************************************************************************/
 int calculate_north_beam_load(void) {
-  int ans = 0;  
+  int ans = 0;
   for (size_t row_idx = 0; row_idx < rows_num; ++row_idx) {
     int row_sum = 0;
-    const char* row = dish[row_idx];
+    const char *row = dish[row_idx];
     for (size_t col_idx = 0; col_idx < cols_num; ++col_idx) {
       row_sum += ('O' == row[col_idx]);
     } /* loop over columns */
     ans += (rows_num - row_idx) * row_sum;
-  }   /* loop over rows */
+  } /* loop over rows */
   return ans;
 } /* calculate_north_beam_load() */
+/******************************************************************************/
+void make_cycle(void) {
+  tilt_dish_north();
+  tilt_dish_west();
+  tilt_dish_south();
+  tilt_dish_east();
+}
 /******************************************************************************/
 void solve_part1(void) {
   /* solution of the part1 */
   PUTS("--------------------");
-  print_dish();
-  PUTS("--------------------");
   tilt_dish_north();
-  print_dish();  
-  PUTS("--------------------");
+  print_dish();
   int ans = calculate_north_beam_load();
   printf("%d\n", ans);
 } /* solve_part1() */
 /******************************************************************************/
 void solve_part2(void) {
-  /* solution of the part2 */
-  printf("%d\n", 0);
+  size_t it;
+
+  for (it = 0; it < 100; ++it) {
+    make_cycle();
+  }
+  size_t it1 = it;
+  memcpy(dish1, dish, sizeof(dish));
+  do {
+    make_cycle();
+    ++it;
+    PRINTF("\e[1;1H\e[2J");
+    print_dish();
+    PRINTF("it=%lu\n", it);
+  } while (memcmp(dish, dish1, sizeof(dish)));
+  const size_t period = it - it1;
+  PRINTF("Cycle detected: %lu\n", period);
+  const size_t target = 1000000000;
+  const size_t iterations_remains = (target - it) % period;
+  for (it = 0; it < iterations_remains; ++it) {
+    make_cycle();
+  }
+  int ans = calculate_north_beam_load();
+  printf("%d\n", ans);
 } /* solve_part2() */
 /******************************************************************************/
 int main(int argc, char *argv[]) {
+  memset(dish, 0, sizeof(dish));
+  memset(dish1, 0, sizeof(dish1));
   const char *input_file_path = (argc > 1) ? argv[1] : DEFAULT_INPUT_FILE;
   if (EXIT_SUCCESS != read_input_data(input_file_path)) {
     perror("Error reading input data from file");
