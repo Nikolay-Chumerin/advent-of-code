f1ddfa3cbf584f74ee405366cb39e5719b937867
Nikolay Chumerin <crappybox@gmail.com>
2023-12-19 22:09:56 +0100

    Add D16P1 solution

--- 2023/Day-16-The-Floor-Will-Be-Lava/solution.c
+++ 2023/Day-16-The-Floor-Will-Be-Lava/solution.c
@@ -8,7 +8,7 @@
 #define DEFAULT_INPUT_FILE "input.txt"
 #define MAX_COLS_NUM (128)
 #define MAX_ROWS_NUM (128)
-
+#define DONT_USE_COLORS
 #ifdef DEBUG
 #define PUTS puts
 #define PRINTF printf
@@ -19,15 +19,44 @@
   {}
 #endif // DEBUG
 
+typedef enum {
+  DIR_UP = '^',
+  DIR_RIGHT = '>',
+  DIR_DOWN = 'v',
+  DIR_LEFT = '<'
+} direction_t;
+
+typedef struct {
+  size_t row;
+  size_t col;
+  direction_t dir;
+} cell_t;
+
+cell_t stack[MAX_ROWS_NUM * MAX_COLS_NUM];
+size_t stack_size = 0;
 size_t rows_num = 0;
 size_t cols_num = 0;
-char contraption[MAX_ROWS_NUM][MAX_COLS_NUM] = {0};
+char map[MAX_ROWS_NUM][MAX_COLS_NUM] = {0};
+bool energized[MAX_ROWS_NUM][MAX_COLS_NUM] = {false};
 int beams[MAX_ROWS_NUM][MAX_COLS_NUM] = {0};
+cell_t beam;
+
 /******************************************************************************/
 #ifdef DEBUG
 void print_data(void) {
   for (size_t row_idx = 0; row_idx < rows_num; ++row_idx) {
-    printf("%s\n", contraption[row_idx]);
+#ifdef USE_COLORS
+    for (size_t col_idx = 0; col_idx < cols_num; ++col_idx) {
+      if (energized[row_idx][col_idx])
+        printf("\033[0;32m");
+      if (col_idx == beam.col && row_idx == beam.row)
+        printf("\033[0;31m");
+      printf("%c\033[0m", map[row_idx][col_idx]);
+    }
+    printf("\n");
+#else
+    printf("%s\n", map[row_idx]);
+#endif
   }
 } /* print_data() */
 #else
@@ -58,7 +87,7 @@ int read_input_data(const char *input_file_path) {
   }
   rows_num = 0;
   while (true) {
-    char *line = contraption[rows_num];
+    char *line = map[rows_num];
     if (!fgets(line, MAX_COLS_NUM, file))
       break;
     const size_t len = trim(line);
@@ -72,10 +101,123 @@ int read_input_data(const char *input_file_path) {
   return EXIT_SUCCESS;
 } /* read_input_data(.) */
 /******************************************************************************/
+bool move(cell_t *cell) {
+  if (!cell)
+    return false;
+  switch (cell->dir) {
+  case DIR_UP:
+    if (cell->row > 0) {
+      --(cell->row);
+      return true;
+    }
+    break;
+  case DIR_DOWN:
+    if (cell->row < rows_num - 1) {
+      ++(cell->row);
+      return true;
+    }
+    break;
+  case DIR_RIGHT:
+    if (cell->col < cols_num - 1) {
+      ++(cell->col);
+      return true;
+    }
+    break;
+  case DIR_LEFT:
+    if (cell->col > 0) {
+      --(cell->col);
+      return true;
+    }
+    break;
+  }
+  return false;
+} /* move(.) */
+/******************************************************************************/
+void reflect(const char mirror, cell_t *beam) {
+  if (!beam)
+    return;
+  switch (beam->dir) {
+  case DIR_UP:
+    beam->dir = ('/' == mirror) ? DIR_RIGHT : DIR_LEFT;
+    break;
+  case DIR_DOWN:
+    beam->dir = ('/' == mirror) ? DIR_LEFT : DIR_RIGHT;
+    break;
+  case DIR_RIGHT:
+    beam->dir = ('/' == mirror) ? DIR_UP : DIR_DOWN;
+    break;
+  case DIR_LEFT:
+    beam->dir = ('/' == mirror) ? DIR_DOWN : DIR_UP;
+    break;
+  } /* switch (beam->dir) */
+}
+/******************************************************************************/
+int count_energized(void) {
+  int ans = 0;
+  for (size_t row_idx = 0; row_idx < rows_num; ++row_idx) {
+    for (size_t col_idx = 0; col_idx < cols_num; ++col_idx) {
+      ans += energized[row_idx][col_idx] ? 1 : 0;
+    }
+  } /* loop over rows */
+  return ans;
+} /* count_energized() */
+/******************************************************************************/
 void solve_part1(void) {
-  /* solution of the part1 */
+  memset(energized, false, sizeof(energized));
+  beam = (cell_t){.row = 0, .col = 0, .dir = DIR_RIGHT};
+  bool moved = false;
+  PRINTF("beam: row=%lu col=%lu dir=%c map[beam]='%c'\n", beam.row, beam.col,
+         beam.dir, map[beam.row][beam.col]);
   print_data();
-  printf("%d\n", 0);
+  do {
+    const char c = map[beam.row][beam.col];
+    switch (c) {
+    case '.':
+      map[beam.row][beam.col] = beam.dir;
+      break;
+    case '\\':
+    case '/':
+      reflect(c, &beam);
+      break;
+    case '|':
+      if ((DIR_DOWN == beam.dir) || (DIR_UP == beam.dir)) { /* ^ v */
+      } else { /* >|  or |< */
+        if (beam.row > 0) {
+          stack[stack_size++] =
+              (cell_t){.row = beam.row - 1, .col = beam.col, .dir = DIR_UP};
+        }
+        beam.dir = DIR_DOWN;
+      }
+      break;
+    case '-':
+      if ((DIR_LEFT == beam.dir) || (DIR_RIGHT == beam.dir)) { /* >- or -< */
+      } else { /* ^- or v- */
+        if (beam.col > 0) {
+          stack[stack_size++] =
+              (cell_t){.row = beam.row, .col = beam.col - 1, .dir = DIR_LEFT};
+        }
+        beam.dir = DIR_RIGHT;
+      }
+    } /* switch() */
+    energized[beam.row][beam.col] = true;
+    moved = move(&beam);
+    const char dir = (char)beam.dir;
+    if (!moved || dir == map[beam.row][beam.col]) {
+      /* stop the current beam and try to pop from stack */
+      if (stack_size) {
+        beam = stack[--stack_size];
+        moved = true;
+      }
+    }
+    PRINTF("beam: row=%lu col=%lu dir=%c map[beam]='%c'\n", beam.row, beam.col,
+           beam.dir, map[beam.row][beam.col]);
+    PRINTF("energized cells num = %d\n", count_energized());
+    PUTS("--------------------------");
+  } while (moved); /* while */
+  print_data();
+  PRINTF("energized cells num = %d\n", count_energized());
+  int ans = count_energized();
+  printf("%d\n", ans);
 } /* solve_part1() */
 /******************************************************************************/
 void solve_part2(void) {
@@ -89,9 +231,7 @@ int main(int argc, char *argv[]) {
     perror("Error reading input data from file");
     return (EXIT_FAILURE);
   }
-
   solve_part1();
   solve_part2();
-
   return EXIT_SUCCESS;
 }
